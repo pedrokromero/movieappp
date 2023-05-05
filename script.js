b3df5c1311cf9896d269bc14da0fba73d7d34f6a
Pedro Borges <121258650+pedrokromero@users.noreply.github.com>
2023-05-05 18:08:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cffb750481c3c2a42d1c84f0121ac947&page=1&language=pt-BR'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=cffb750481c3c2a42d1c84f0121ac947&language=pt-BR&query="'
+
+const form = document.getElementById ('form')
+const search = document.getElementById ('search')
+const main = document.getElementById ('main')
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res= await fetch(url)
+    const data = await res.json()
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+    movies.forEach((movie) =>{
+        const { title, poster_path, vote_average, overview } = movie
+        const movieEL = document.createElement('div')
+        movieEL.classList.add('movie')
+
+        movieEL.innerHTML = `
+                 <img src="${IMG_PATH + poster_path}" alt="${title}">
+                 <div class="info">
+                    <h3>${title}</h3>
+                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                 </div>
+            <div class="overview">
+                <h3>Sinopse</h3>
+                ${overview}
+            </div> `
+        main.appendChild(movieEL)
+    })
+}
+
+function getClassByRate(vote){
+    if(vote >= 7){
+        return 'green'
+    } else if (vote >= 5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const searchTerm = search.value
+    if(searchTerm && searchTerm !==''){
+        getMovies(SEARCH_API + searchTerm)
+        search.value = ''
+    } else {
+        window.location = reload()
+    }
+})
